d85385c93537b58fb4ee333603dc7e1fdb71bf2b
pdh112 <pdiegohernandez@gmail.com>
2023-01-10 15:28:33 -0500

    v2 changes

---
+++ src/com/coderscampus/Assignment2Application.java
@@ -0,0 +1,15 @@
+package com.coderscampus;
+
+public class Assignment2Application {
+
+	public static void main(String[] args) {
+
+		// generate a random number
+		
+		//and collect user input
+		
+		//etc
+
+	}
+
+}
